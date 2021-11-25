5d66a731cef13a7da1ee6275418fbd38c054001d
Astral2001 <laiannguyen2001@gmail.com>
2021-11-25 12:41:06 +0700

    Complete

--- user/urls.py
+++ user/urls.py
@@ -5,10 +5,11 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 from . import views
+
 urlpatterns=[
     path('login/',LoginView.as_view(template_name='user/login.html',authentication_form=AuthenticationForm),name='login'),
     url(r"^register$",views.Register_User,name= 'register'),
     path('logout/',LogoutView.as_view(),name='logout'),
-    path('change_password/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('change_password'),template_name="user/change_password.html"), name='change_password'),
+    path('change_password/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('login'),template_name="user/change_password.html"), name='change_password'),
     url(r"^edit_profile$",views.edit,name='edit_profile')
 ]
